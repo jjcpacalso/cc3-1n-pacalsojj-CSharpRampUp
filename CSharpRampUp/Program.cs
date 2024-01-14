8574b64c7afb165423f57313a854dbb3a2b0c586
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2024-01-14 11:04:25 +0800

    Add project files.

---
+++ CSharpRampUp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpRampUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
